a8c767862fd0c0026e32c3c3a757fd723c38c416
Vic <forsphone@gmai.com>
2016-05-07 10:48:43 +0800

    First commit

---
+++ SharedProject1/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject1
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
